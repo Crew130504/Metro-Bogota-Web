4cc88d5e2e09d89814f3a442f0bbd29af4df429b
StevenSza <01apothicon01@gmail.com>
2023-12-03 14:28:58 -0500

    sentencias sql

--- Final/src/java/Modelo/RouteVO.java
+++ Final/src/java/Modelo/RouteVO.java
@@ -1,29 +1,94 @@
 package Modelo;
 
 public class RouteVO {
-    private String name;
-    private String description;
-    private String image;
-    private String schedule;
-    private String day;
 
-    public RouteVO(String name, String description, String image, String schedule, String day) {
+    
+    private String name;
+    private String color;
+    private String description;
+    private String originDestination;
+    private String day;
+    private String schedule;
+    private String url;
+
+
+    public RouteVO(String name, String color, String description, String originDestination, String url) {
         this.name = name;
+        this.color = color;
         this.description = description;
-        this.image = image;
+        this.originDestination = originDestination;
+        this.url = url;
+    }
+
+    public RouteVO(String name, String color, String day, String schedule) {
+        this.name = name;
+        this.color = color;
+        this.day = day;
         this.schedule = schedule;
+    }
+
+   
+  
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getOriginDestination() {
+        return originDestination;
+    }
+
+    public void setOriginDestination(String originDestination) {
+        this.originDestination = originDestination;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public void setDay(String day) {
         this.day = day;
     }
 
-    /*
-    
-    public RouteVO(String name, String description, String image) {
-        this.name = name;
-        this.description = description;
-        this.image = image;
+    public String getSchedule() {
+        return schedule;
+    }
+
+    public void setSchedule(String schedule) {
+        this.schedule = schedule;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
     }
     
-    */
+
+
+
     
     
     public RouteVO(String name, String schedule, String day) {
@@ -35,45 +100,7 @@ public class RouteVO {
     
     
     
-    public String getSchedule() {
-        return schedule;
-    }
-
-    public void setSchedule(String schedule) {
-        this.schedule = schedule;
-    }
-
-    public String getDay() {
-        return day;
-    }
-
-    public void setDay(String day) {
-        this.day = day;
-    }    
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getImage() {
-        return image;
-    }
-
-    public void setImage(String image) {
-        this.image = image;
-    }
+    
     
     
 }
